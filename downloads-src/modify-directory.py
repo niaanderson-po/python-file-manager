f4b06d347875ca9e8a12ee5e61f90784bd7c997d
niaanderson-po <niaanderson.po@gmail.com>
2025-01-16 23:53:23 -0500

    refactor: add error handling

--- downloads-src/modify-directory.py
+++ downloads-src/modify-directory.py
@@ -20,7 +20,9 @@ for directory, subdir_list, file_list in os.walk(root):
             if file_extension in extensions:
                 source_name = os.path.join(directory, name)
                 target_name = os.path.join(f'/Users/niaapple/Downloads/{folder}', name)
-                print(f'Moving: {source_name} to: {target_name}')
-                os.rename(source_name, target_name)
-            break
+                try:
+                    print(f'Moving: {source_name} to: {target_name}')
+                    os.rename(source_name, target_name)
+                except Exception as e:
+                    print(f"Error moving {source_name} to {target_name}: {e}")
     break
