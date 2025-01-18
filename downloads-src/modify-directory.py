8f4a8cf1b4cd1cd086cf84eccd7bb15ae4c90a41
niaanderson-po <niaanderson.po@gmail.com>
2025-01-17 19:28:43 -0500

    refactor: encapsulate file organization logic in a function

--- downloads-src/modify-directory.py
+++ downloads-src/modify-directory.py
@@ -1,28 +1,31 @@
 import os
 
-root = os.path.join("/Users/niaapple", "Downloads")
+def organize_downloads(root):
+    file_type_map = {
+            ('gif', 'png', 'PNG', 'jpeg', 'jpg', 'heic', 'HEIC', 'webp', 'avif'): 'Images',
+            ('pdf',): 'PDF',
+            ('ics', 'docx', 'epub'): 'Documents',
+            ('xml', 'html'): 'Developer',
+            ('pptx',): 'Presentation',
+            ('dmg', 'zip'): 'Other'
+        }
 
-file_type_map = {
-        ('gif', 'png', 'PNG', 'jpeg', 'jpg', 'heic', 'HEIC', 'webp', 'avif'): 'Images',
-        ('pdf',): 'PDF',
-        ('ics', 'docx', 'epub'): 'Documents',
-        ('xml', 'html'): 'Developer',
-        ('pptx',): 'Presentation',
-        ('dmg', 'zip'): 'Other'
-    }
+    for directory, subdir_list, file_list in os.walk(root):
+        for name in file_list:
+            file_extension = name.split('.')[-1].lower()
 
-for directory, subdir_list, file_list in os.walk(root):
-    for name in file_list:
-        file_extension = name.split('.')[-1].lower()
+            #relocate unsorted downloads by file extension type
+            for extensions, folder in file_type_map.items():
+                if file_extension in extensions:
+                    source_name = os.path.join(directory, name)
+                    target_name = os.path.join(f'/Users/niaapple/Downloads/{folder}', name)
+                    try:
+                        print(f'Moving: {source_name} to: {target_name}')
+                        os.rename(source_name, target_name)
+                    except Exception as e:
+                        print(f"Error moving {source_name} to {target_name}: {e}")
+        break
 
-        #relocate unsorted downloads by file extension type
-        for extensions, folder in file_type_map.items():
-            if file_extension in extensions:
-                source_name = os.path.join(directory, name)
-                target_name = os.path.join(f'/Users/niaapple/Downloads/{folder}', name)
-                try:
-                    print(f'Moving: {source_name} to: {target_name}')
-                    os.rename(source_name, target_name)
-                except Exception as e:
-                    print(f"Error moving {source_name} to {target_name}: {e}")
-    break
+if __name__ == "__main__":
+    root = "/Users/niaapple/Downloads"
+    organize_downloads(root)
